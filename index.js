8b6b19a9c2cbbcc52bd5a6d58d19c3aa894bf37a
venla <venla.e.rantanen@gmail.com>
2023-09-11 13:02:06 +0300

    Task 3.8

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const morgan = require("morgan");
-const app = express();
+const { token } = require("morgan");
 
+const app = express();
 app.use(express.json());
-app.use(morgan("tiny"));
+
+app.use(
+  morgan(
+    ":method :url :status :total-time[digits] :req[header] :response-time ms :postData"
+  )
+);
+morgan.token("postData", function (req, res, param) {
+  return JSON.stringify(req.body);
+});
 
 let persons = [
   {
