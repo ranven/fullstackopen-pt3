5c04f3e199967aa86474d84f58a56085f049a907
venla <venla.e.rantanen@gmail.com>
2023-09-13 15:31:51 +0300

    Task 3.22

--- index.js
+++ index.js
@@ -1,122 +1,120 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const { token } = require("morgan");
-
-const app = express();
-const Contact = require("./models/contact");
-app.use(cors());
-app.use(express.static("dist"));
-app.use(express.json());
+require("dotenv").config()
+const express = require("express")
+const morgan = require("morgan")
+const cors = require("cors")
+const app = express()
+const Contact = require("./models/contact")
+app.use(cors())
+app.use(express.static("dist"))
+app.use(express.json())
 app.use(
   morgan(
     ":method :url :status :total-time[digits] :req[header] :response-time ms :postData"
   )
-);
-morgan.token("postData", function (req, res, param) {
-  return JSON.stringify(req.body);
-});
+)
+morgan.token("postData", function (req) {
+  return JSON.stringify(req.body)
+})
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
-});
+  res.send("<h1>Hello World!</h1>")
+})
 
 app.get("/info", (req, res) => {
   Contact.find({}).then((contacts) => {
     res.send(
       `<p>Phonebook has info for ${contacts.length} people</p><p>${Date()}</p>`
-    );
-  });
-});
+    )
+  })
+})
 
 app.get("/api/persons", (req, res) => {
   Contact.find({}).then((contacts) => {
-    res.json(contacts);
-  });
-});
+    res.json(contacts)
+  })
+})
 
 app.get("/api/persons/:id", (req, res, next) => {
   Contact.findById(req.params.id)
     .then((contact) => {
       if (contact) {
-        res.json(contact);
+        res.json(contact)
       } else {
-        res.status(404).end();
+        res.status(404).end()
       }
     })
-    .catch((err) => next(err));
-});
+    .catch((err) => next(err))
+})
 
 app.put("/api/persons/:id", (req, res, next) => {
   const person = {
     name: req.body.name,
     number: req.body.number,
-  };
+  }
   Contact.findByIdAndUpdate(req.params.id, person, {
     new: true,
     runValidators: true,
     context: "query",
   })
     .then((updatedContact) => {
-      res.json(updatedContact);
+      res.json(updatedContact)
     })
-    .catch((err) => next(err));
-});
+    .catch((err) => next(err))
+})
 
 app.delete("/api/persons/:id", (req, res, next) => {
   Contact.findByIdAndRemove(req.params.id)
-    .then((result) => {
-      res.status(204).end();
+    .then(() => {
+      res.status(204).end()
     })
-    .catch((err) => next(err));
-});
+    .catch((err) => next(err))
+})
 
 app.post("/api/persons", (req, res, next) => {
-  const body = req.body;
+  const body = req.body
 
   if (!body.name) {
     return res.status(400).json({
       error: "name missing",
-    });
+    })
   } else if (!body.number) {
     return res.status(400).json({
       error: "number missing",
-    });
+    })
   }
   const contact = new Contact({
     name: body.name,
     number: body.number,
-  });
+  })
   contact
     .save()
     .then((saved) => {
-      res.json(saved);
+      res.json(saved)
     })
-    .catch((err) => next(err));
-});
+    .catch((err) => next(err))
+})
 
 const errorHandler = (err, req, res, next) => {
-  console.error(err.message);
+  console.error(err.message)
 
   if (err.name === "CastError") {
-    return res.status(400).send({ error: "malformatted id" });
+    return res.status(400).send({ error: "malformatted id" })
   } else if (err.name === "ValidationError") {
-    return res.status(400).json({ error: err.message });
+    return res.status(400).json({ error: err.message })
   }
 
-  next(err);
-};
+  next(err)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
 
 const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: "unknown endpoint" });
-};
+  res.status(404).send({ error: "unknown endpoint" })
+}
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- models/contact.js
+++ models/contact.js
@@ -1,16 +1,16 @@
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
+const mongoose = require("mongoose")
+mongoose.set("strictQuery", false)
 
-const url = process.env.DB_URL;
+const url = process.env.DB_URL
 
 mongoose
   .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB");
+  .then(() => {
+    console.log("connected to MongoDB")
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log("error connecting to MongoDB:", error.message)
+  })
 
 const contactSchema = new mongoose.Schema({
   name: {
@@ -23,20 +23,20 @@ const contactSchema = new mongoose.Schema({
     minlength: 8,
     validate: {
       validator: function (val) {
-        return /^\d{2,3}-\d{7,}/.test(val);
+        return /^\d{2,3}-\d{7,}/.test(val)
       },
       message: (props) => `${props.value} doesn't meet the requirements`,
     },
     required: true,
   },
-});
+})
 
 contactSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Contact", contactSchema);
+module.exports = mongoose.model("Contact", contactSchema)
